9060b77310519f09eb957e52ecc8d4775df30ab2
Emily Patsko <Emily.Patsko@softwire.com>
2021-09-16 14:54:52 +0100

    feat: Part 2 and create gitignore

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace FizzBuzz_CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
