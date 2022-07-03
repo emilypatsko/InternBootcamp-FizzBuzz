640832e3f094ff12f40e779f1758faa15e637199
Emily Patsko <emily.patsko@softwire.com>
2022-07-03 16:15:14 +0100

    refactor: Reorder method definitions

--- Program.cs
+++ Program.cs
@@ -6,6 +6,60 @@ namespace FizzBuzz_CSharp
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            FizzBuzz(args);
+
+            // Single line version
+            // Enumerable.Range(1, 100).ToList().ForEach(i => Console.WriteLine(i % 3 != 0 && i % 5 != 0 && i % 7 != 0 ? i.ToString() : $"{(i % 3 == 0 ? "Fizz" : "")}{(i % 5 == 0 ? "Buzz" : "")}{(i % 7 == 0 ? "Bang" : "")}"));
+        }
+
+        static void FizzBuzz(string[] args)
+        {
+            Dictionary<string, bool> rules = new Dictionary<string, bool>
+            {
+                { "fizz", true },
+                { "buzz", true },
+                { "bang", true },
+                { "bong", true },
+                { "fezz", true },
+                { "reverse", true },
+            };
+            int length = 100;
+
+            if (args.Length > 0)
+            {
+                length = Int32.TryParse(args[0], out length) ? length : 100;
+
+                if (args.Length > 1)
+                {
+                    foreach (var rule in rules)
+                    {
+                        if (Array.IndexOf(args, rule.Key) > -1)
+                        {
+                            rules[rule.Key] = true;
+                        }
+                        else
+                        {
+                            rules[rule.Key] = false;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 1; i <= length; i++)
+            {
+                List<string> thingToPrint = new List<string>();
+                if (rules["fizz"]) FizzRule(i, thingToPrint);
+                if (rules["buzz"]) BuzzRule(i, thingToPrint);
+                if (rules["bang"]) BangRule(i, thingToPrint);
+                if (rules["bong"]) BongRule(i, thingToPrint);
+                if (rules["fezz"]) FezzRule(i, thingToPrint);
+                if (rules["reverse"]) ReverseRule(i, thingToPrint);
+                Console.WriteLine(!thingToPrint.Any() ? i : string.Join("", thingToPrint));
+            }
+        }
+
         static void FizzRule(int num, List<string> thingToPrint)
         {
             if (num % 3 == 0)
@@ -61,60 +115,6 @@ namespace FizzBuzz_CSharp
             {
                 thingToPrint.Reverse();
             }
-        }
-
-        static void FizzBuzz(string[] args)
-        {
-            Dictionary<string, bool> rules = new Dictionary<string, bool>
-            {
-                { "fizz", true },
-                { "buzz", true },
-                { "bang", true },
-                { "bong", true },
-                { "fezz", true },
-                { "reverse", true },
-            };
-            int length = 100;
-
-            if (args.Length > 0)
-            {
-                length = Int32.TryParse(args[0], out length) ? length : 100;
-            
-                if (args.Length > 1)
-                {
-                    foreach (var rule in rules)
-                    {
-                        if (Array.IndexOf(args, rule.Key) > -1)
-                        {
-                            rules[rule.Key] = true;
-                        }
-                        else 
-                        {
-                            rules[rule.Key] = false;
-                        }
-                    }
-                }                
-            }
-
-            for (int i = 1; i <= length; i++)
-            {
-                List<string> thingToPrint = new List<string>();
-                if (rules["fizz"]) FizzRule(i, thingToPrint);
-                if (rules["buzz"]) BuzzRule(i, thingToPrint);
-                if (rules["bang"]) BangRule(i, thingToPrint);
-                if (rules["bong"]) BongRule(i, thingToPrint);
-                if (rules["fezz"]) FezzRule(i, thingToPrint);
-                if (rules["reverse"]) ReverseRule(i, thingToPrint);
-                Console.WriteLine(!thingToPrint.Any() ? i : string.Join("", thingToPrint));
-            }
-        }
-        
-        static void Main(string[] args)
-        {
-            FizzBuzz(args);
-
-            // Single line version
-            // Enumerable.Range(1, 100).ToList().ForEach(i => Console.WriteLine(i % 3 != 0 && i % 5 != 0 && i % 7 != 0 ? i.ToString() : $"{(i % 3 == 0 ? "Fizz" : "")}{(i % 5 == 0 ? "Buzz" : "")}{(i % 7 == 0 ? "Bang" : "")}"));
         }
     }
 }
